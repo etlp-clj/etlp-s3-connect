911dc4550cfef6b667bbe2a413ddd726e1000359
Rahul Gaur <rahul.nbg@gmail.com>
2023-06-30 02:44:18 +0530

    chore: version release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.aregee/etlp-s3-connect "0.1.0-SNAPSHOT"
+(defproject org.clojars.aregee/etlp-s3-connect "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -9,5 +9,5 @@
                  [com.cognitect.aws/api "0.8.635"]
                  [org.clojure/tools.logging "1.2.4"]
                  [org.clojure/core.async "0.4.500"]
-                 [com.github.aregee/etlp "0.4.2-SNAPSHOT"]]
+                 [org.clojars.aregee/etlp "0.3.2-SNAPSHOT"]]
   :repl-options {:init-ns etlp-s3-connect.core})
